29f0cacb4f68a18bd41c966e4978e401a05b4968
stjambou <stephan.jambou@gmail.com>
2025-04-02 21:57:22 +0200

    Dev

---
+++ src/api/ordering/controllers/ordering.ts
@@ -0,0 +1,7 @@
+/**
+ * ordering controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::ordering.ordering');
---
+++ src/api/ordering/services/ordering.ts
@@ -0,0 +1,7 @@
+/**
+ * ordering service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::ordering.ordering');
